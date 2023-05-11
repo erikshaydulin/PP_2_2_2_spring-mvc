4808b9bab270716e89479fa805cb6fc38e421f03
MikeStrat <erikshaydulin@gmail.com>
2023-05-11 11:52:10 +0500

    CarServiceImpl, CarController (bugFixes)

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,11 @@ import java.util.List;
 @Controller
 public class CarController {
 
+    @Autowired
+    private CarService carService;
+
     @GetMapping("/cars")
     public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
-        CarService carService = new CarServiceImpl();
 
         Integer limit = null;
         if (count != null) {
